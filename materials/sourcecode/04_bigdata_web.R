dbb407cc6ac85ac7f36da4a8125d24e93aa1f597
umatter <umatter@protonmail.com>
2019-09-11 17:05:22 +0200

    update sourcecode

--- materials/sourcecode/04_bigdata_web.R
+++ materials/sourcecode/04_bigdata_web.R
@@ -1,3 +1,28 @@
+## ---- echo=FALSE, results='asis', warning=FALSE--------------------------
+# conditional on the output format of the whole document,
+# generate and render a HTML or a LaTeX table.
+if (knitr::is_latex_output()) {
+  
+  cat('
+  \\begin{center}
+  \\href{http://creativecommons.org/licenses/by-nc-sa/4.0/}{\\includegraphics[width = .1\\textwidth]{../img/cc.png}}
+  
+  \\smallskip
+  
+  This work is licensed under a \\href{http://creativecommons.org/licenses/by-nc-sa/4.0/}{Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License}
+  \\end{center}
+  '
+  )
+  
+} else {
+     cat('
+     
+   <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License</a>.
+   
+')
+}
+
+
 ## <?xml version="1.0"?>
 
 ##   <temperatures>
