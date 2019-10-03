e0766c2ad7f4efe3fcc710d2606e599ae14c2740
umatter <umatter@protonmail.com>
2019-10-03 09:43:16 +0200

    add simple code example to illustrate floating point problems

---
+++ materials/sourcecode/floatingpoint_problems.R
@@ -0,0 +1,19 @@
+# define three variables each with the value 1/3
+a <- 1/3
+b <- 1/3
+c <- 1/3
+
+# use those variables to compute 2/3 in different ways
+# 1)
+1 - a
+# 2)
+b + c
+
+# from looking at the definition of a, b, and c 
+# 1) and 2) must be mathematically identical
+# after all 2/3 = 2/3...
+
+# However
+(1-a) == (b+c)
+(1-a) - (b+c)
+
