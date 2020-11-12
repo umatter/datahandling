60653def51381ca429b30fd68c1c24c734ff5be4
Ulrich Matter <ulrich.matter@unibas.ch>
2020-11-12 10:02:55 +0100

    update recompiled materials

--- materials/sourcecode/07_data_import.R
+++ materials/sourcecode/07_data_import.R
@@ -207,3 +207,69 @@ xml_text(customer_names)
 ## 
 ## 
 
+
+
+
+## ----echo=FALSE-----------------------------------------------------------------------------------
+dgov <- data.table::fread("../../data/divided_gov.csv")
+head(dgov)
+
+
+
+
+## ----echo=FALSE, warning=FALSE, message=FALSE-----------------------------------------------------
+# SETUP ------------------
+
+# load packages
+library(rvest)
+library(tidyverse)
+
+# fix vars
+SOURCE_PATH <- "https://en.wikipedia.org/wiki/Divided_government_in_the_United_States"
+OUTPUT_PATH <- "../../data/divided_gov.csv"
+
+
+
+## -------------------------------------------------------------------------------------------------
+# FETCH/FORMAT DATA -------------------
+
+# fetch from web
+doc <- read_html(SOURCE_PATH)
+html_tables <- html_node(doc, xpath = "//table[@class='wikitable']")
+tab <- html_table(html_tables)
+tab <- tab[-nrow(tab), ] # remove last row (not containing data)
+
+
+## -------------------------------------------------------------------------------------------------
+# generate year-level data.frame
+
+# prepare loop
+all_years <- list() # the container
+n <- length(tab$Year) # number of cases to iterate through
+length(all_years) <- n
+# generate year-level observations. row by row.
+for (i in 1:n){
+      # select row
+      row <- tab[i,] 
+      y <- row$Year
+      # 
+      begin <- as.numeric(unlist(strsplit(x = y, split = "[\\–\\-]", perl = TRUE))[1])
+      end <- as.numeric(unlist(strsplit(x = y, split =  "[\\–\\-]"))[2])
+      tabnew <- data.frame(year=begin:(end-1), president=row$President, senate=row$Senate, house=row$House)
+      all_years[[i]] <- tabnew # store in container
+}
+
+# stack all rows together
+allyears <- bind_rows(all_years)
+
+
+## -------------------------------------------------------------------------------------------------
+# WRITE TO DISK --------------------------------------------------
+
+# inspect
+head(allyears)
+
+# write to csv
+write_csv(allyears, path=OUTPUT_PATH)
+
+
